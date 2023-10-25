c1c0e91c0bed51fc1087f16113130210b494a27f
Afonso Canalle Jr <afonso.canallejr@gmail.com>
2023-10-25 09:56:06 -0300

    alterado os with + teste no js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,3 +15,27 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove("menu-expended")
 }
+
+window.addEventListener('resize', function() {
+    adjustMarginTop();
+  });
+  
+  window.addEventListener('scroll', function() {
+    adjustMarginTop();
+  });
+  
+  function adjustMarginTop() {
+    var windowHeight = window.innerHeight;
+    var documentHeight = document.documentElement.clientHeight;
+    var addressBarHeight = windowHeight - documentHeight;
+  
+    // Defina uma margem superior no elemento principal do conteúdo
+    var contentElement = document.getElementById('#navigation'); // Substitua 'seu-conteudo' pelo ID do elemento de conteúdo
+    if (contentElement) {
+      contentElement.style.marginTop = addressBarHeight + 'px';
+    }
+  }
+  
+  // Chame a função inicialmente para configurar a margem corretamente
+  adjustMarginTop();
+  
