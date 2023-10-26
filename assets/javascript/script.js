baf1378d901174284aa7ce274a10f4dd65b1b451
Afonso Canalle Jr <afonso.canallejr@gmail.com>
2023-10-26 12:17:32 -0300

    Só deus na causa

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,43 +16,17 @@ function closeMenu() {
     document.body.classList.remove("menu-expended")
 }
 
-window.addEventListener('resize', function() {
-    adjustMarginTop();
-  });
-  
-  window.addEventListener('scroll', function() {
-    adjustMarginTop();
-  });
-  
-  function adjustMarginTop() {
-    var windowHeight = window.innerHeight;
-    var documentHeight = document.documentElement.clientHeight;
-    var addressBarHeight = windowHeight - documentHeight;
-  
-    // Defina uma margem superior no elemento principal do conteúdo
-    var contentElement = document.getElementById('navigation'); // Substitua 'seu-conteudo' pelo ID do elemento de conteúdo
-    if (contentElement) {
-      contentElement.style.marginTop = addressBarHeight + 'px';
-    }
-  }
-  
-  // Chame a função inicialmente para configurar a margem corretamente
-  adjustMarginTop();
-
-  /* Ajuste a margem superior do conteúdo para considerar a barra de endereço */
 function adjustForAddressBar() {
-    const contentElement = document.getElementById('#navigation'); // Substitua 'seu-conteudo' pelo ID do elemento de conteúdo
-    const addressBarHeight = window.innerHeight - document.documentElement.clientHeight;
-    
-    if (contentElement) {
-      contentElement.style.marginTop = addressBarHeight + 'px';
-      contentElement.style.marginLeft = 'auto';
-      contentElement.style.marginRight = 'auto';
-    }
+  const contentElement = document.getElementById('navigation'); // Substitua 'navigation' pelo ID do elemento de conteúdo
+  const addressBarHeight = window.innerHeight - document.documentElement.clientHeight;
+
+  if (contentElement) {
+    contentElement.style.marginTop = addressBarHeight + 'px';
+    contentElement.style.marginLeft = 'auto';
+    contentElement.style.marginRight = 'auto';
   }
-  
-  // Chame a função inicialmente e sempre que a janela for redimensionada
-  adjustForAddressBar();
-  window.addEventListener('resize', adjustForAddressBar);
-  
-  
+}
+
+// Chame a função inicialmente e sempre que a janela for redimensionada
+adjustForAddressBar();
+window.addEventListener('resize', adjustForAddressBar);
