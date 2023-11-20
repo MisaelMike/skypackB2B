eb0531e424723f81e976b5238ed313fbf92ca028
Afonso Canalle Jr <afonso.canallejr@gmail.com>
2023-11-20 09:39:29 -0300

    Finalizado celular

---
+++ produtos/assets/js/script.js
@@ -0,0 +1,54 @@
+function onScroll() {
+    if (scrollY > 0) {
+        let colocarClasse = document.querySelector("#navigation");
+        colocarClasse.classList.add('scroll')
+    } else {
+        let retirarClasse = document.querySelector("#navigation");
+        retirarClasse.classList.remove('scroll')
+    }
+}
+
+function openMenu() {
+    // document.documentElement.scrollTop = 0; // Isso rolará a página para o topo
+    document.body.classList.add("menu-expended")
+}
+
+function closeMenu() {
+    document.body.classList.remove("menu-expended")
+}
+
+function adjustForAddressBar() {
+  const contentElement = document.getElementById('navigation'); // 
+  let addressBarHeight = window.innerHeight - document.documentElement.clientHeight;
+
+  // Verificar se a barra de endereço está visível e ajustar a altura
+  if (addressBarHeight > 0) {
+    addressBarHeight = 0;
+  }
+
+  if (contentElement) {
+    contentElement.style.marginTop = addressBarHeight + 'px';
+    contentElement.style.marginLeft = 'auto';
+    contentElement.style.marginRight = 'auto';
+  }
+}
+
+// Chame a função inicialmente e sempre que a janela for redimensionada
+adjustForAddressBar();
+window.addEventListener('resize', adjustForAddressBar);
+
+document.querySelectorAll('.scroll-link').forEach(element => {
+  element.addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      
+      if (targetElement) {
+          const yOffset = -100;
+          const y = targetElement.getBoundingClientRect().top + window.scrollY + yOffset;
+          
+          window.scrollTo({ top: y, behavior: 'smooth' });
+      }
+  });
+});
