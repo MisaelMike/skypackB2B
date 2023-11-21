cd1da80e9daa107183a64f10afd597773d68ef02
Afonso Canalle Jr <afonso.canallejr@gmail.com>
2023-11-21 14:21:24 -0300

    Começo do desktop, finalizado o nav

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,28 @@
+window.addEventListener('scroll', onScroll)
+
 function onScroll() {
-    if (scrollY > 0) {
-        let colocarClasse = document.querySelector("#navigation");
-        colocarClasse.classList.add('scroll')
-    } else {
-        let retirarClasse = document.querySelector("#navigation");
-        retirarClasse.classList.remove('scroll')
-    }
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()  
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    let colocarClasse = document.querySelector("#navigation");
+    colocarClasse.classList.add('scroll')
+  } else {
+    let retirarClasse = document.querySelector("#navigation");
+    retirarClasse.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 600) {
+    let colocarClasseBackToTopButton = document.querySelector("#backToTopButton");
+    colocarClasseBackToTopButton.classList.add('show')
+  } else {
+    let retirarClasseBackToTopButton = document.querySelector("#backToTopButton");
+    retirarClasseBackToTopButton.classList.remove('show')
+  }
 }
 
 function openMenu() {
@@ -33,9 +50,32 @@ function adjustForAddressBar() {
   }
 }
 
-// Chame a função inicialmente e sempre que a janela for redimensionada
-adjustForAddressBar();
-window.addEventListener('resize', adjustForAddressBar);
+function toggleContent() {
+  var button = document.querySelector('.button-menu');
+  if (window.innerWidth < 1024) {
+    button.textContent = 'Fale com um especialista';
+  } else {
+    button.textContent = 'Contato';
+  }
+}
+
+function updateContent() {
+  if (window.innerWidth >= 1024) {
+    toggleContent("Contato");
+  } else {
+      toggleContent("Fale com um especialista");
+  }
+  adjustForAddressBar();
+}
+
+function toggleAndAdjust() {
+  toggleContent();
+  updateContent();
+  onScroll()
+}
+
+window.addEventListener('resize', toggleAndAdjust);
+document.addEventListener('DOMContentLoaded', toggleAndAdjust);
 
 document.querySelectorAll('.scroll-link').forEach(element => {
   element.addEventListener('click', function(e) {
